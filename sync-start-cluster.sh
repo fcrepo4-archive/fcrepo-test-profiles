0a37d1ab5d4432c5a9939bf58b111c3927d238e6
Gregory Jansen <count0@email.unc.edu>
2013-12-09 15:42:27 -0500

    script now contains an array of node hosts removed all required settings from settings.xml files, put them in script as system properties

--- sync-start-cluster.sh
+++ sync-start-cluster.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
-CLUSTER_PREFIX=Fedora4-
+hosts[0]=Fedora4-Master.libint.unc.edu # used as initial host for jgroups
+hosts[1]=Fedora4-1.libint.unc.edu
+hosts[2]=Fedora4-2.libint.unc.edu
+hosts[3]=Fedora4-3.libint.unc.edu
+hosts[4]=Fedora4-4.libint.unc.edu
+hosts[5]=Fedora4-5.libint.unc.edu
+hosts[6]=Fedora4-6.libint.unc.edu
+hosts[7]=Fedora4-7.libint.unc.edu
+
 CLUSTER_SIZE=3
-INSTALL_DIR=/data
+INSTALL_DIR=/data # you need write permissions
+STORE_FAST=target/stores # if you move these stores, you must clean them up
+STORE_SLOW=target/stores
+INITIAL_IP=`nslookup ${hosts[0]} | tail -2 | awk -F ": " '{print $2}'`
 
-# clean local project
-killall java
-sleep 20
-cd $INSTALL_DIR/fcrepo-test-profiles
-mvn clean
-mvn -U package
+mvn -U clean package
 
-# Shutdown local and remote services
-for i in {1..3}
+for i in ${hosts[*]}
 do
-  echo "STOPPING $CLUSTER_PREFIX$i"
-  ssh $CLUSTER_PREFIX$i "killall java"
-  sleep 20
-  echo "SYNCING $CLUSTER_PREFIX$i"
-  rsync -ar $INSTALL_DIR/fcrepo-test-profiles $CLUSTER_PREFIX$i:$INSTALL_DIR
-  rsync -ar ~/.m2 $CLUSTER_PREFIX$i:~
+  echo STOPPING $i
+  ssh $i "killall java"
 done
 
-cd $INSTALL_DIR/fcrepo-test-profiles
-mvn -P $1 -Dhost.ip=172.27.244.223 compile cargo:run > $INSTALL_DIR/out.log 2> $INSTALL_DIR/err.log < /dev/null &
-
-for i in {1..3}
+for i in ${hosts[*]}
 do
-  echo "STARTING $CLUSTER_PREFIX$i"
-#  IP=`nslookup Fedora4-1 | tail -2 | awk -F ": " '{print $2}'`
-  IP=$CLUSTER_PREFIX$i
-  ssh $CLUSTER_PREFIX$i "cd $INSTALL_DIR/fcrepo-test-profiles; nohup mvn -U -P $1 -Dhost.ip=$IP clean compile cargo:run > $INSTALL_DIR/out.log 2> $INSTALL_DIR/err.log < /dev/null &"
+  echo "SYNCING $i"
+  rsync -ar . $i:$INSTALL_DIR/fcrepo-test-profiles
+  ssh $i "rm -rf --preserve-root $INSTALL_DIR/fcrepo-test-profiles/target"
 done
 
-sleep 20
+for i in $(seq 0 $CLUSTER_SIZE)
+do
+  echo "STARTING ${hosts[$i]}"
+  IP=`nslookup ${hosts[$i]} | tail -2 | awk -F ": " '{print $2}'`
+#  IP=${hosts[$i]}
+  ssh ${hosts[$i]} "cd $INSTALL_DIR/fcrepo-test-profiles; nohup mvn -U -P $1 -Dhost.ip=$IP -Dfcrepo.store.fast=$STORE_FAST -Dfcrepo.store.slow=$STORE_SLOW -Djgroups.tcpping.initial_hosts=$INITIAL_IP[7800] clean compile cargo:run > $INSTALL_DIR/out.log 2> $INSTALL_DIR/err.log < /dev/null &"
+done
 
-cd $INSTALL_DIR/fcrepo-test-profiles
-mvn -P $1,workflow-profile -Dhost.ip=172.27.244.223 verify
+echo "$CLUSTER_SIZE nodes started with profiles: $1"
